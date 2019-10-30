0bc2ccd6e3a996fc4d5113945b62908b53ea8222
dylrob34 <dylana1998@gmail.com>
2019-10-30 12:42:31 -0400

    Hard coded key just for testing

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -17,10 +17,12 @@ class Main extends React.Component {
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
+                authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbXBsb3llZV9pZCI6IjEyMzQiLCJpYXQiOjE1NzI0NTIwMzIsImV4cCI6MTU3MjQ1OTIzMn0.UG14XaPPvmlZD3AtoddNLHwrhYvuEAOBaEUL-DLKbIw"
               }
         })
         .then((response) => response.json())
         .then((data) => {
+            console.log("data:", data);
             console.log("name", data.name);
             this.setState({name:data.name});
         })
@@ -30,7 +32,7 @@ class Main extends React.Component {
 
         return (
           <div className="mainDiv">
-            <Navigation name={this.props.name}/>
+            <Navigation name={this.state.name}/>
             <Selector />
             <Calendar />
 
