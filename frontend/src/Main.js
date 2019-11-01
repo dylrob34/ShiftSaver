9866373351092f77a63c2f520a7927d5c49257fa
Dylan <dylana1998@gmail.com>
2019-11-01 19:39:05 -0400

    Worked on figuring out this calendar. lol

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -27,12 +27,19 @@ class Main extends React.Component {
         })
     }
 
+    test() {
+        console.log("testing");
+        
+    }
+
     render() {
         return (
           <div className="mainDiv">
             <Navigation name={this.state.name}/>
             <Selector />
             <Calendar />
+
+            <button onClick={this.test}>test this shit out</button>
 
             <div className="upcomingShiftsDiv">
                 <h1>Upcoming Shifts</h1>
