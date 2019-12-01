f4da7897df8921ff1bf386c492928a4d6b2bc847
Anastasiia <ababenko@my.bridgeport.edu>
2019-11-30 23:07:56 -0500

    employees in People's section are clickable now. Upon click it shows buttons Email and AssignShift

--- frontend/src/People.jsx
+++ frontend/src/People.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {userToken} from './Login';
-import {updateLoginState} from './MessageStore';
+import './static/css/People.css';
+import { userToken } from './Login';
+import { updateLoginState } from './MessageStore';
 
 
 
@@ -13,61 +14,84 @@ class People extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            current:"",
-            allPeople:""
+            current: "",
+            allPeople: "",
+            isSelected: false
         };
 
         this.changePerson = this.changePerson.bind(this);
+        this.selectPerson = this.selectPerson.bind(this);
+
+
     }
 
-changePerson(person) {
-    this.setState({current:person});
-}
-
-componentDidMount() {
-    fetch("http://localhost/user/getAllPeople", {
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            authorization: "Bearer " + userToken 
-          }
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        if (data.loggedIn === false) {
-            updateLoginState(false);
-        } else {
-            this.setState({allPeople:data});
-        }
-    })
-}
-
-getInfo(){
-    
-}
-
-render() {
-
-    var list = [];
-    
-    if (this.state.allPeople.length > 0)
-    {
-        for (var i = 0; i < this.state.allPeople.length; i++) {
-            var text = this.state.allPeople[i].first_name + " " + this.state.allPeople[i].last_name +
-            "\n" + this.state.allPeople[i].phone_number
-            list.push(<li key={i}><button onClick = {this.getInfo}>{text}</button></li>)
-        }
+    changePerson(person) {
+        this.setState({ current : person});
+        this.selectPerson();
     }
 
-    return(
-        <div>
-            <h1>People!</h1>
-            <ul>
-                {list}
-            </ul>
-        </div>
-    );
-}
+    componentDidMount() {
+        fetch("http://localhost/user/getAllPeople", {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                authorization: "Bearer " + userToken
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.loggedIn === false) {
+                    updateLoginState(false);
+                } else {
+                    this.setState({ allPeople: data });
+                }
+            })
+    }
+
+    selectPerson() {
+        this.setState({ isSelected: true });
+    }
+
+    email() {
+        //alert(this.state.current)
+        //var JSONobj = JSON.parse(this.state.current);
+        //alert(JSONobj.first_name);
+        
+    }
+
+    assignShift() {
+        
+    }
+
+
+    render() {
+
+        var list = [];
+
+        if (this.state.allPeople.length > 0) {
+            for (var i = 0; i < this.state.allPeople.length; i++) {
+                var text = this.state.allPeople[i].first_name + " " + this.state.allPeople[i].last_name +
+                    "\n" + this.state.allPeople[i].phone_number
+               // list.push(<li key={i}><button onClick={() => this.changePerson(JSON.stringify(this.state.allPeople[document.getElementsByTagName('ul').closest('li').attr("key")]))}>{text}</button></li >)
+                list.push(<li key={i}><button onClick={this.selectPerson}>{text}</button></li >)
+            }
+        }
+
+        return (
+                <div>
+                <h1>People!</h1>
+                <ul>
+                    {list}
+                </ul>
+                
+                <div className={this.state.isSelected ? "show" : "invisible"} >
+                    <p>Contact or assign shifts to selected employee:</p>
+                    <button onClick={this.email}>Email</button>
+                    <button onClick={this.assignShift}>Assign Shifts</button>                    
+                    </div>
+                </div>
+        );
+    }
 
 }
 
